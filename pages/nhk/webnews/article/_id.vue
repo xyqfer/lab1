da3f4cbf9808a2e635f65def942c8cb38d88f58f
凉凉 <xyqfer@gmail.com>
2019-10-01 16:06:16 +0800

    update

---
+++ pages/nhk/webnews/article/_id.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <Render0>
+            <template #title>
+                {{title}}
+            </template>
+            <template #content>
+                <div>
+                    {{title}}
+                </div>
+                <div class="news-content" v-html="htmlContent"></div>
+                <WordList :words="wordList"></WordList>
+            </template>
+        </Render0>
+    </div>
+</template>
+
+<script>
+import Render0 from '~/components/Render0.vue';
+import WordList from '~/components/WordList.vue';
+
+export default {
+    components: {
+        Render0,
+        WordList,
+    },
+
+    layout: 'archive0',
+
+    head() {
+        return {
+            title: this.title,
+        };
+    },
+
+    async asyncData({ params, $axios }) {
+        const { id } = params;
+        const { data } = await $axios({
+            method: 'get',
+            url: `${process.env.API_HOST}/api/v1/nhk/webnews/article/${id}`,
+        });
+        const { title, htmlContent, wordList, } = data.data;
+
+        return {
+            title, 
+            htmlContent, 
+            wordList,
+        };
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.news-content {
+    margin-top: 20px;
+
+    &__item {
+        margin-top: 10px;
+    }
+
+    &__input {
+        width: 100%;
+        box-sizing: border-box;
+    }
+}
+</style>
---
+++ pages/nhk/webnews/list.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <Render0>
+            <template #title>
+                {{title}}
+            </template>
+            <template #content>
+                <div class="link-container" v-for="item in listData" :key="item.objectId">
+                    <a :href="`/nhk/webnews/article/${item.objectId}`" target="_blank">
+                        <h4>
+                            {{item.title}}
+                        </h4>
+                    </a>
+                </div>
+            </template>
+        </Render0>
+    </div>
+</template>
+
+<script>
+import Render0 from '~/components/Render0.vue';
+
+export default {
+    components: {
+        Render0,
+    },
+
+    layout: 'archive0',
+
+    data() {
+        return {
+            title: 'NHK-WebNews',
+        };
+    },
+
+    head() {
+        return {
+            title: this.title,
+        };
+    },
+
+    async asyncData({ params, query, $axios }) {
+        const { data } = await $axios({
+            method: 'get',
+            url: `${process.env.API_HOST}/api/v1/nhk/webnews`,
+        });
+
+        return {
+            listData: data.data.list,
+        };
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.link-container {
+    margin-bottom: 30px;
+}
+</style>
