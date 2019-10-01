c694e04e3b8d9103543d3ee41fa4924e94d17bc1
凉凉 <xyqfer@gmail.com>
2019-10-01 11:02:23 +0800

    update

---
+++ pages/nhk/easynews/list.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <Render0>
+            <template #title>
+                {{title}}
+            </template>
+            <template #content>
+                
+            </template>
+        </Render0>
+    </div>
+</template>
+
+<script>
+import Render0 from '~/components/Render0.vue';
+
+export default {
+    components: {
+        Render0,
+    },
+
+    layout: 'archive0',
+
+    data() {
+        return {
+            title: 'NHK-EasyNews',
+        };
+    },
+
+    head() {
+        return {
+            title: this.title,
+        };
+    },
+
+    async asyncData({params, query, $axios}) {
+        const { data } = await $axios({
+            method: 'get',
+            url: `${process.env.API_HOST}/api/v1/nhk/easynews`,
+        });
+        const { htmlContentList, wordList} = data.data;
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
