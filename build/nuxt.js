0c4184931794daf3d30a932ec848bb2a885ea7d5
凉凉 <xyqfer@gmail.com>
2019-10-07 17:15:07 +0800

    is this can work?

--- build/nuxt.js
+++ build/nuxt.js
@@ -1,21 +1,28 @@
-const { execSync } = require('child_process');
+const { exec } = require('child_process');
 const { Nuxt, Builder } = require('nuxt');
 const config = require('../nuxt.config');
 config.dev = process.env.NODE_ENV !== 'production';
 
 module.exports = async (context) => {
+    console.log('start build nuxt');
+    const initNuxt = async () => {
+        const nuxt = new Nuxt(config)
+        if (config.dev) {
+            const builder = new Builder(nuxt)
+            await builder.build()
+        } else {
+            await nuxt.ready()
+        }
+    };
+
     if (process.env.NODE_ENV === 'production') {
-        execSync('npm run build');
+        exec('npm run build', async () => {
+            await initNuxt();
+        });
+    } else {
+        await initNuxt();
     }
 
-    const nuxt = new Nuxt(config)
-    if (config.dev) {
-        const builder = new Builder(nuxt)
-        await builder.build()
-    } else {
-        await nuxt.ready()
-    }
-    
     context.use(nuxt.render);
-    console.log('nuxt build finished');
+    console.log('finish build nuxt');
 };
