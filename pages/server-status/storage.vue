c751380c90949b11ac6485edaa6edd2695b8a74c
凉凉 <xyqfer@gmail.com>
2019-10-13 22:16:22 +0800

    添加对比

--- pages/server-status/storage.vue
+++ pages/server-status/storage.vue
@@ -6,14 +6,14 @@
             </template>
             <template #content>
                 <div class="date">
-                    {{date}} 共 {{totalCount}}
+                    {{date}} 共 {{totalCount}} +<span :class="{red: totalCountChange > 0}">{{totalCountChange}}</span>
                 </div>
                 <div class="list-container" v-for="item in listData" :key="item.name">
                     <div class="list-item list-item1">
                         {{item.name}}
                     </div>
                     <div class="list-item list-item2">
-                        {{item.count}}
+                        {{item.count}} +<span :class="{red: item.change > 0}">{{item.change}}</span>
                     </div>
                     <div class="list-item list-item3">
                         {{(item.percent * 100).toFixed(4)}}%
@@ -24,7 +24,7 @@
 
                     </div>
                     <div class="list-item list-item2">
-                        {{totalCount}}
+                        {{totalCount}} +<span :class="{red: totalCountChange > 0}">{{totalCountChange}}</span>
                     </div>
                     <div class="list-item list-item3">
                         
@@ -53,12 +53,36 @@ export default {
             url: `${process.env.API_HOST}/api/v1/server-status/storage`,
         });
         const { date, list, totalCount, } = data.data;
+        const listData = list.reverse().map((item) => {
+            item.change = 0;
+            return item;
+        });
 
         return {
             date,
-            listData: data.data.list.reverse(),
+            listData,
             totalCount,
+            totalCountChange: 0,
         };
+    },
+
+    mounted() {
+        let previousData = localStorage.getItem(this.date);
+        if (previousData) {
+            const { listData, totalCount, } = JSON.parse(previousData);
+            this.totalCountChange = this.totalCount - totalCount;
+            this.listData.forEach((item) => {
+                const previousItem = listData.find(({name}) => {
+                    return name === item.name;
+                });
+                item.change = item.count - previousItem.count;
+            });
+        }
+
+        localStorage.setItem(this.date, JSON.stringify({
+            listData: this.listData,
+            totalCount: this.totalCount,
+        }));
     },
 }
 </script>
@@ -73,23 +97,27 @@ export default {
 }
 
 .list-item {
-    flex: 2;
-
     &.list-item1 {
+        flex: 2;
         text-align: left;
     }
 
     &.list-item2 {
         flex: 1;
-        text-align: center;
+        text-align: right;
     }
 
     &.list-item3 {
+        flex: 1.4;
         text-align: right;
     }
 }
 
 .total-count {
     margin-top: 20px;
+}
+
+.red {
+    color: red;
 }
 </style>
