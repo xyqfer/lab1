150eaf73368269f27c77521f20a88173255a0bad
凉凉 <xyqfer@gmail.com>
2019-10-13 10:43:07 +0800

    update build

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 'use strict';
 
-const { initCloudEngine } = require('@xyqfer/init-leancloud-engine');
-initCloudEngine();
+const AV = require('leanengine');
 
-const getPort = require('@xyqfer/get-port');
-const app = require('./server');
+if (process.env.NODE_ENV === 'production') {
+  AV.init({
+    appId: process.env.LEANCLOUD_APP_ID,
+    appKey: process.env.LEANCLOUD_APP_KEY,
+    masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
+  });
+  
+  // 如果不希望使用 masterKey 权限，可以将下面一行删除
+  AV.Cloud.useMasterKey();
+}
+
+const createApp = require('./server');
 
 (async () => {
-  const PORT = await getPort();
+  const app = await createApp();
 
-  app.listen(PORT, async function (err) {
+  // 端口一定要从环境变量 `LEANCLOUD_APP_PORT` 中获取。
+  // LeanEngine 运行时会分配端口并赋值到该变量。
+  const PORT = parseInt(process.env.LEANCLOUD_APP_PORT || process.env.PORT || 3000);
+
+  app.listen(PORT, function (err) {
     console.log('Node app is running on port:', PORT);
-
-    require('./build/nuxt')(app);
 
     // 注册全局未捕获异常处理器
     process.on('uncaughtException', function(err) {
